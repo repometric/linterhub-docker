4af7e6a1f1d6b38c55ae636d9c2b7a7f1b88d1bf
Nalin Bhardwaj <maverick.nalin@gmail.com>
2018-01-08 23:35:52 +0530

    Add scancode engine

---
+++ engines/scancode/tests/run_compare.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+actual=$(sh run_tests.sh)
+expected=$(cat expected.txt)
+
+if [ "$actual" = "$expected" ]
+then
+  echo "OK"
+else
+  echo -e "ERROR\n Actual:"$actual"\nExpected:"$expected
+  exit 1
+fi
